0bfca2bfe453b0f41d3e94dacabad16daa78c7fb
Vyacheslav <govras99@outlook.com>
2025-04-06 14:31:17 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo pacman -S hyprland hyprpaper hyprlock hyprpolkitagent hyprshot xdg-desktop-portal-hyprland wofi waybar \
+pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse \
+power-profiles-daemon \
+networkmanager network-manager-applet nm-connection-editor \
+linux-headers dkms nvidia-open-dkms nvidia-utils \
+firefix nautilus nwg-look-bin
